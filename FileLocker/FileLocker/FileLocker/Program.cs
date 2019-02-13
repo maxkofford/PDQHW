2757785650668d1e2f00f62df0340222cd053f01
Maxwell G Kofford <maxkofford@gmail.com>
2019-02-12 19:03:12 -0700

    Added a small project for locking files on windows

---
+++ FileLocker/FileLocker/FileLocker/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileLocker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream s2 = new FileStream(args[0], FileMode.Open, FileAccess.Read, FileShare.None))
+            {
+            
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
